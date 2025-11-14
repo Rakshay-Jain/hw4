bb83a63dfed1ab4f63b5eb96ab4343f8658edad5
Rakshay Jain <rakshayj@usc.edu>
2025-11-14 05:08:31 +0000

    first part

--- equal-paths.cpp
+++ equal-paths.cpp
@@ -13,6 +13,39 @@ using namespace std;
 bool equalPaths(Node * root)
 {
     // Add your code below
+    if (root == nullptr) {
+        return true;
+    }
+    if (root->left == nullptr && root->right == nullptr) {
+        return true;
+    }
+
+    if(root->right == nullptr){
+        return equalPaths(root->left);
+    }
+    if(root->left == nullptr){
+        return equalPaths(root->right);
+    }
+
+    int left_depth = depth(root->left);
+    int right_depth = depth(root->right);
+    if (left_depth != right_depth) {
+        return false;
+    }
+    return equalPaths(root->left) && equalPaths(root->right);
+    
 
 }
 
+static int depth(Node* node) {
+    if (node == nullptr) {
+        return 0;
+    }
+    int l =depth(node->left);
+    int r =depth(node->right);
+    if (l>r){
+        return l+1;
+    }
+    return r+1;
+
+}
